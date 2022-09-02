e42bbe650f52445031b92853b3923c423037f4df
yagya17 <77620760+yagya17@users.noreply.github.com>
2022-09-02 10:08:57 +0530

    Update 114-flatten-binary-tree-to-linked-list.cpp

--- 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.cpp
+++ 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.cpp
@@ -29,3 +29,21 @@ public:
         
     }
 };
+
+
+class Solution {
+public:
+  TreeNode* prev=NULL;
+    void flatten(TreeNode* root) {
+        
+        if(!root) return;
+        
+        flatten(root->right);
+       flatten(root->left);  
+        root->right=prev;
+        root->left=NULL;
+        prev=root;
+        
+        
+    }
+};
