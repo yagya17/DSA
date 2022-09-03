e9f87fd2523b28e37399d07a6b91c99b8f26bb91
yagya17 <77620760+yagya17@users.noreply.github.com>
2022-09-03 11:01:28 +0530

    Time: 51 ms (5.96%), Space: 16.7 MB (96.68%) - LeetHub

--- 116-populating-next-right-pointers-in-each-node/116-populating-next-right-pointers-in-each-node.cpp
+++ 116-populating-next-right-pointers-in-each-node/116-populating-next-right-pointers-in-each-node.cpp
@@ -19,28 +19,16 @@ public:
 class Solution {
 public:
     Node* connect(Node* root) {
-        if(!root) return root;
-      queue<pair<Node*,int>>q;
-        root->next=NULL;
-       if(root->left) q.push({root->left,0});
-        if(root->right)q.push({root->right,0});
-        while(!q.empty())
+     
+      if(!root) return root;
+        if(root->left)
         {
-           Node* cur= q.front().first; 
-            int lvl=q.front().second;
-            while(q.front().second==lvl)
-            {
-                Node *temp=q.front().first;
-                q.pop(); 
-                if(temp->left)q.push({temp->left,lvl+1});
-                if(temp->right)q.push({temp->right,lvl+1});
-                if(q.front().second==lvl)
-                {temp->next=q.front().first;}
-                else{temp->next=NULL;}
-                
-            }
-            
+            root->left->next=root->right;
         }
+        if(root->right&&root->next)  root->right->next=root->next->left;
+        connect(root->left);
+        connect(root->right);
+        
         return root;
     }
 };
