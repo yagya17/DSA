36b424a7634e4731c066614ddfdf6b6a35644b8e
yagya17 <77620760+yagya17@users.noreply.github.com>
2022-09-03 11:20:28 +0530

    Create 116-populating-next-right-pointers-in-each-node.cpp

--- 116-populating-next-right-pointers-in-each-node/116-populating-next-right-pointers-in-each-node.cpp
+++ 116-populating-next-right-pointers-in-each-node/116-populating-next-right-pointers-in-each-node.cpp
@@ -19,6 +19,64 @@ public:
 class Solution {
 public:
     Node* connect(Node* root) {
+        if(!root) return root;
+      queue<pair<Node*,int>>q;
+        root->next=NULL;
+       if(root->left) q.push({root->left,0});
+        if(root->right)q.push({root->right,0});
+        while(!q.empty())
+        {
+           Node* cur= q.front().first; 
+            int lvl=q.front().second;
+            while(q.front().second==lvl)
+            {
+                Node *temp=q.front().first;
+                q.pop(); 
+                if(temp->left)q.push({temp->left,lvl+1});
+                if(temp->right)q.push({temp->right,lvl+1});
+                if(q.front().second==lvl)
+                {temp->next=q.front().first;}
+                else{temp->next=NULL;}
+                
+            }
+            
+        }
+        return root;
+    }
+};
+
+T.C. : O(N)
+S.C. : O(N)
+    
+Recursive:
+
+class Solution {
+public:
+    Node* connect(Node* root) {
+     
+      if(!root) return root;
+        if(root->left)
+        {
+            root->left->next=root->right;
+        }
+        if(root->right&&root->next)  root->right->next=root->next->left;
+        connect(root->left);
+        connect(root->right);
+        
+        return root;
+    }
+};
+
+T.C. : O(N)
+S.C. : O(N) (recursion)
+    
+    
+Iterative:
+
+
+class Solution {
+public:
+    Node* connect(Node* root) {
     Node*  prev=root;Node* cur;
       while(prev)
       { cur=prev;
@@ -34,3 +92,7 @@ public:
         return root;
     }
 };
+
+
+T.C. : O(N)
+S.C. : O(1) 
