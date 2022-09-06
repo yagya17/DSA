98569d2a40642d2dd5bbac7aa7dcb6bbc0e039ab
yagya17 <77620760+yagya17@users.noreply.github.com>
2022-09-06 16:10:28 +0530

    Update 230-kth-smallest-element-in-a-bst.cpp

--- 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.cpp
+++ 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.cpp
@@ -58,6 +58,28 @@ S.C. : O(N)
     
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     
-    
+ ITERATIVE:
+class Solution {
+public:
+   
+    int kthSmallest(TreeNode* root, int k) {
+        stack<TreeNode*>st;
+        while(root||!st.empty())
+        {
+        while(root)
+        {
+            st.push(root);
+            root=root->left;
+        }
+        root=st.top();st.pop();
+        k--;
+        if(k==0) return root->val;
+        root=root->right;
+        }
+        return -1;
+    }
+};
+/////////////////////////////////////////////////////////////////
+
     
     
