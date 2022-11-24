d71e2cfabaf31736807894321420bfe8af71189e
yagya17 <77620760+yagya17@users.noreply.github.com>
2022-11-24 20:32:54 +0530

    Time: 817 ms (21.51%), Space: 18.1 MB (7.13%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -1,16 +1,19 @@
 class Solution {
 public:
+    
+     int sol(vector<int>& nums,int i, vector<int>&t)
+    {   
+        if(i>=nums.size()-1) return 0;
+        if(t[i]!=10001) return t[i];
+         
+        for(int j=1;j<=nums[i];j++)
+        {
+            t[i]=min(t[i],1+sol(nums,i+j,t));
+        }
+        return t[i];
+    }
     int jump(vector<int>& nums) {
-	vector<int> dp(size(nums), 10001); // initialise all to max possible jumps + 1 denoting dp[i] hasn't been computed yet
-	return solve(nums, dp, 0);
-}
-// recursive solver to find min jumps to reach end
-int solve(vector<int>& nums, vector<int>& dp, int pos) {
-	if(pos >= size(nums) - 1) return 0;    // when we reach end, return 0 denoting no more jumps required
-	if(dp[pos] != 10001) return dp[pos];    // number of jumps from pos to end is already calculated, so just return it
-	// explore all possible jump sizes from current position. Store & return min jumps required
-	for(int j = 1; j <= nums[pos]; j++)
-		dp[pos] = min(dp[pos], 1 + solve(nums, dp, pos + j));        
-	return dp[pos];
-}
+        vector<int>t(nums.size(),10001);
+        return sol(nums,0,t);
+    }
 };
