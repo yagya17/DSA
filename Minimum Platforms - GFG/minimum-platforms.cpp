34e679f131ad4e630be79b5e5ccac69ecd2b043e
yagya17 <77620760+yagya17@users.noreply.github.com>
2023-11-19 23:18:18 +0530

    Added solution - LeetHub

--- Minimum Platforms - GFG/minimum-platforms.cpp
+++ Minimum Platforms - GFG/minimum-platforms.cpp
@@ -15,22 +15,18 @@ class Solution{
     sort(arr,arr+n);
     sort(dep,dep+n);
  
-    int ans=1;
-    int count=1;
-    int i=1,j=0;
+    int ans=1; int cnt=1;
+    int i=1; int j=0;
     while(i<n && j<n)
     {
-        if(arr[i]<=dep[j]) //one more platform needed
+        if(arr[i]<=dep[j])
         {
-            count++;
-            i++;
+            cnt++; i++;
         }
-        else //one platform can be reduced
-        {
-            count--;
-            j++;
+        else{
+            cnt--; j++;
         }
-        ans=max(ans,count); //updating the value with the current maximum
+        ans=max(ans, cnt);
     }
     return ans;
     }
